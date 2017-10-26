62b492eb0473d5b4f4781289faf0ecac00022f87
Rebecca <becky@rebeccas-imac.local>
2017-10-26 13:26:23 -0400

    Initial Commit

---
+++ xcode8github/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xcode8github
+//
+//  Created by Rebecca on 10/26/17.
+//  Copyright © 2017 Rebecca. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xcode8github/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xcode8github
+//
+//  Created by Rebecca on 10/26/17.
+//  Copyright © 2017 Rebecca. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
